66b7375a614e6175655d310e9da9e6a04da2e1dd
juampidemarco <juampidemarco@gmail.com>
2021-05-28 18:01:34 -0300

    Version inicial Proyecto Tacunet Desktop

---
+++ src/main/java/com/tacunet/Tacunet_Spring_desktop/App.java
@@ -0,0 +1,13 @@
+package com.tacunet.Tacunet_Spring_desktop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
